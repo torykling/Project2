9aaf45ab1b8ff2c5920f565efd08f9b48c6d5837
Tory Klingenstein <toryklingenstein@gmail.com>
2019-10-10 17:38:01 -0400

    changing deck

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
+// const cards = {
+// 	hearts: [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14],
+// 	spades: [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14],
+// 	clubs: [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14],
+// 	diamonds: [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
+// }
+
+// const deck = cards.hearts.concat(cards.spades, cards.clubs, cards.diamonds);
+
+// original deck - no suit or face value
 let deck = [2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 6, 6, 6, 6, 7, 7, 7, 7, 8, 8, 8, 8, 9, 9, 9, 9, 10, 10, 10, 10, 11, 11, 11, 11, 12, 12, 12, 12, 13, 13, 13, 13, 14, 14, 14, 14]
 // this function shuffles the deck randomly- found a Fisher-Yates shuffle on javascript info
 function shuffle() {
@@ -9,14 +19,13 @@ function shuffle() {
 // this function splits the deck in two and deals to players
 function dealCards() {
 	for(let m = 0; m < deck.length; m++) {
-		if (m % 2 !== 0) {
+		if (m % 2 === 0) {
 			player1.push(deck[m]);
 		} else {
 			player2.push(deck[m]);
 		}
 	}
 }
-
 // this starts the game
 function startGame() {
 	shuffle();
@@ -31,10 +40,9 @@ let player2 = []
 let thePot = []
 
 // this function moves the top card to the pot
-function oneCardToPot(player) {
-	thePot.push(player.shift());
-}
-
+const oneCardToPot = (player) => thePot.push(player.shift());
+const player1Card = thePot[thePot.length-2];
+const player2Card = thePot[thePot.length-1];
 // this function pushes items from the pot one a time into a player's array AND deletes the item from the pot
 function pushPotItems(player) {
 	for (let i = 0; i < thePot.length; i++) {
@@ -44,16 +52,15 @@ function pushPotItems(player) {
 }
 
 // this function creates an alert to update the user about the results of the round
-function roundAlert(winner) {
-	alert(`End of round. ${winner} wins. Player 1 has ${player1.length} cards. Player 2 has ${player2.length} cards.`);
-}
+const roundAlert = (winner) => alert(` Player 1 played a ${player1Card}. Player 2 played a ${player2Card}. End of round. ${winner} wins! Player 1 has ${player1.length} cards. Player 2 has ${player2.length} cards.`);
+
 
 // this function checks to see if the game is over
 function gameOver(winner) {
 	if (player1.length === 0) {
-		alert('Game over. Player 1 is out of cards. Player 2 wins! Reload the page to play again!');
+		alert('Game over. Player 1 is out of cards. Player 2 wins the game!!! Reload the page to play again!');
 	} else if (player2.length === 0) {
-		alert('Game over. Player 2 is out of cards. Player 1 wins! Reload the page to play again!');
+		alert('Game over. Player 2 is out of cards. Player 1 wins the game!!! Reload the page to play again!');
 	} else {
 		roundAlert(winner);
 	}
